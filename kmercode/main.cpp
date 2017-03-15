49b33b8df76f0a16c07e5a51873b1077fe7524d5
aydozz <aydinbuluc@gmail.com>
2017-03-14 20:14:26 -0700

    seems to work

--- kmercode/main.cpp
+++ kmercode/main.cpp
@@ -51,10 +51,41 @@ int main(int argc, char* argv[])
 {
     Kmer::set_k(KMER_LENGTH);
     vector<filedata> allfiles = GetFiles(argv[1]);
+    size_t upperlimit = 1000;   // a thousand reads at a time
     
+    vector<string> seqs;
+    vector<string> quals;
     for(auto itr= allfiles.begin(); itr != allfiles.end(); itr++)
     {
         ParallelFASTQ *pfq = new ParallelFASTQ();
         pfq->open(itr->filename, false, itr->filesize);
+        
+        size_t fillstatus = 1;
+        while(fillstatus)
+        {
+            fillstatus = pfq->fill_block(seqs, quals, upperlimit);
+            size_t nreads = seqs.size();
+            
+            for(size_t i=0; i< nreads; ++i)
+            {
+                size_t found;
+                found = seqs[i].length();
+                
+                // skip this sequence if the length is too short
+                if (seqs[i].length() <= KMER_LENGTH) {
+                    continue;
+                }
+                int nkmers = (seqs[i].length()-KMER_LENGTH+1);
+
+                
+                std::vector<Kmer> kmers = Kmer::getKmers(seqs[i]); // calculate all the kmers
+                assert(kmers.size() == nkmers);
+                
+                // Aydin: the following lines are just for debugging, comment out after you get a feeling of how it works
+                cout << "printing the first k-mer and its reverse complement from this read: ";
+                cout << kmers[0].toString() << " " << kmers[0].twin().toString() << endl;
+            }
+            
+        }
     }
 }
