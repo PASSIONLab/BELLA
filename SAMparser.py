808db847d4b18178af057439398694758dabd7d0
Giulia Guidi <gl.guidi93@gmail.com>
2018-03-18 13:45:39 +0100

    python code to move from sam file to a format usable in the evaluation code (real dataset)

--- SAMparser.py
+++ SAMparser.py
@@ -1,19 +1,20 @@
-from simplesam import Reader, Writer, Sam
+from simplesam import Reader, Writer
 import inspect
 import sys, os, fileinput, string
 
 in_file = open(sys.argv[1], 'r')
 in_sam = Reader(in_file)
-out_file = open('PBcR-PB-ec-Q10-strand.txt', 'w')
+out_file = open('groundTruth.txt', 'w')
 # out_sam = Writer(out_file)
 
 x = next(in_sam)
 
-while(x != ''):
-	if(x.reverse):
-		out_file.write("+" + " ")
-	else:
-		out_file.write("-" + " ")
+while(x.qname != ''):
+	#if(x.reverse):
+	#	out_file.write("+" + " ")
+	#else:
+	#	out_file.write("-" + " ")
+	out_file.write(x.rname + " ")
 	out_file.write(x.qname + " ")
 	out_file.write(str(x.pos) + " ")
 	out_file.write(str(x.pos + len(x.seq)) + "\n")
