c84cebda56bc8764d1f3d5dc1d4946d946bb8246
Giulia Guidi <gl.guidi93@gmail.com>
2018-03-30 07:08:52 +0200

    space instead of tab to get dibella output

--- analysis/evaluation.h
+++ analysis/evaluation.h
@@ -325,16 +325,16 @@ void benchmarkingAl(ifstream & groundtruth, ifstream & bella, ifstream & minimap
             string colName, rowName, nkmer, score;
             string colStart, colEnd, colLen, rowStart, rowEnd, rowLen;
             
-            getline(lineStream, colName, '\t');
-            getline(lineStream, rowName, '\t');
-            getline(lineStream, nkmer, '\t');
-            getline(lineStream, score, '\t');
-            getline(lineStream, colStart, '\t');
-            getline(lineStream, colEnd, '\t');
-            getline(lineStream, colLen, '\t');
-            getline(lineStream, rowStart, '\t');
-            getline(lineStream, rowEnd, '\t');
-            getline(lineStream, rowLen, '\t');
+            getline(lineStream, colName, ' ');
+            getline(lineStream, rowName, ' ');
+            getline(lineStream, nkmer, ' ');
+            getline(lineStream, score, ' ');
+            getline(lineStream, colStart, ' ');
+            getline(lineStream, colEnd, ' ');
+            getline(lineStream, colLen, ' ');
+            getline(lineStream, rowStart, ' ');
+            getline(lineStream, rowEnd, ' ');
+            getline(lineStream, rowLen, ' ');
 
             if(colName != rowName) // to be sure to not count self aligned pairs
             {
@@ -516,15 +516,15 @@ void benchmarkingAl(ifstream & groundtruth, ifstream & bella, ifstream & minimap
             string colName, rowName, colStart, colEnd, rowLen, colLen;
             string strand, rowStart, rowEnd;
     
-            getline(lineStream, colName, ' ' );
-            getline(lineStream, colLen, ' ' );
-            getline(lineStream, colStart, ' ' );
-            getline(lineStream, colEnd, ' ' );
-            getline(lineStream, strand, ' ' );
-            getline(lineStream, rowName, ' ' );
-            getline(lineStream, rowLen, ' ' );
-            getline(lineStream, rowStart, ' ' );
-            getline(lineStream, rowEnd, ' ' );
+            getline(lineStream, colName, '\t' );
+            getline(lineStream, colLen, '\t' );
+            getline(lineStream, colStart, '\t' );
+            getline(lineStream, colEnd, '\t' );
+            getline(lineStream, strand, '\t' );
+            getline(lineStream, rowName, '\t' );
+            getline(lineStream, rowLen, '\t' );
+            getline(lineStream, rowStart, '\t' );
+            getline(lineStream, rowEnd, '\t' );
     
             colName = "@" + colName;
             rowName = "@" + rowName;
