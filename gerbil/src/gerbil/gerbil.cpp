ae9231a549d678c2c65ae51fd274d1d8844db98c
Giulia Guidi <gl.guidi93@gmail.com>
2019-07-25 10:39:47 -0700

    paty

---
+++ gerbil/src/gerbil/gerbil.cpp
@@ -0,0 +1,13 @@
+/*
+#include "../../include/gerbil/Application.h"
+
+using namespace gerbil;
+
+int main() {
+
+	//Application
+	Application application(17,*//*"/global/homes/q/qizhou/bella/paeruginosa30x_0001_5reads.fastq"*//*"/global/cscratch1/sd/qizhou/synthetic_datasets/abaumannii30x_0001.fastq","tempDir",0,"outputTRY",false);
+	application.process();
+
+	return 0;
+}*/
