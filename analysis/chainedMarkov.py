eab38e370839e09ab2ce872f28be631b7b803dec
Giulia Guidi <gl.guidi93@gmail.com>
2020-01-25 19:28:33 -0800

    updated nested mc

--- analysis/chainedMarkov.py
+++ analysis/chainedMarkov.py
@@ -37,25 +37,54 @@ def func(L, p, k):
         wk = wk + w[k]
     return np.linalg.matrix_power(transixMat, L)[0, k], wk
 
+def nmc(L, p, k, w):
+    states = w*k+1
+    transixMat = np.zeros((states, states))
+    for i in range(w):
+        for j in range(k):
+            transixMat[j+i*k, i*k]       = (1-p*p)
+            transixMat[j+i*k, j+i*k + 1] = p*p      
+    transixMat[k*w, k*w] = 1.0
+    # print(transixMat)
+    return np.linalg.matrix_power(transixMat, L)[0, w*k]
+
 ksize=17
 pcorr=0.85
 
-# p1, p2 = func(L=2000, p=pcorr, k=ksize)
-# print(p1, p2)
+p = nmc(L=2500, p=pcorr, k=ksize, w=2)
+print(p)
 
-probs = [func(L=i, p=pcorr, k=ksize) for i in range(500, 15005, 500)]
-probs = np.asarray(probs)
-# print(probs)
-# print(probs[:,0], probs[:,1])
+# probs1 = [nmc(L=i, p=pcorr, k=ksize, w=1) for i in range(2000, 13005, 500)]
+# probs1 = np.asarray(probs1)
 
-plt.plot(np.arange(500, 15005, 500), probs[:,0], color='black', label='BMC')
-plt.plot(np.arange(500, 15005, 500), probs[:,1], color='red',   label='2-states NMC')
-# plt.plot(np.arange(1000, 30005, 1000), probs[:,0]**2, color='blue', label='BMC**2')
-plt.title('Nested Markov Chain')
-plt.xlabel('Overlap Length')
-plt.ylabel('Cumulative Probability')
-plt.legend()
-plt.show()
+# probs2 = [nmc(L=i, p=pcorr, k=ksize, w=2) for i in range(2000, 13005, 500)]
+# probs2 = np.asarray(probs2)
+
+# probs3 = [nmc(L=i, p=pcorr, k=ksize, w=3) for i in range(2000, 13005, 500)]
+# probs3 = np.asarray(probs3)
+
+# probs4 = [nmc(L=i, p=pcorr, k=ksize, w=4) for i in range(2000, 13005, 500)]
+# probs4 = np.asarray(probs4)
+
+# probs5 = [nmc(L=i, p=pcorr, k=ksize, w=5) for i in range(2000, 13005, 500)]
+# probs5 = np.asarray(probs5)
+
+# probs6 = [nmc(L=i, p=pcorr, k=ksize, w=6) for i in range(2000, 13005, 500)]
+# probs6 = np.asarray(probs6)
+
+# plt.hlines(0.90, 2000, 13000, colors='red', linestyles='--', label='0.90')
+
+# plt.plot(np.arange(2000, 13005, 500), probs1, color='black',        label='BMC')
+# plt.plot(np.arange(2000, 13005, 500), probs2, color='royalblue',    label='2-states NMC')
+# plt.plot(np.arange(2000, 13005, 500), probs3, color='darkorchid',   label='3-states NMC')
+# plt.plot(np.arange(2000, 13005, 500), probs4, color='green',        label='4-states NMC')
+# plt.plot(np.arange(2000, 13005, 500), probs5, color='darkorange',   label='5-states NMC')
+# plt.plot(np.arange(2000, 13005, 500), probs6, color='firebrick',    label='6-states NMC')
+# plt.title('Nested Markov Chain')
+# plt.xlabel('Overlap Length')
+# plt.ylabel('Cumulative Probability')
+# plt.legend()
+# plt.show()
 
 #
 # End of program
