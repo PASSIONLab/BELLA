481f759165f4886572dc88cc014fa78fe7ee838b
Giulia Guidi <gl.guidi93@gmail.com>
2020-01-27 10:49:21 -0800

    start nmc integration

--- myMarkov.h
+++ myMarkov.h
@@ -20,9 +20,72 @@
 
 #include "mtspgemm2017/common.h"
 
+// modified from https://www.geeksforgeeks.org/interval-tree/
+struct Interval 
+{ 
+    int lower, upper;
+}; 
+  
+// node struct in interval search tree 
+struct ITNode 
+{
+    Interval *i;
+    int max, num; 
+    ITNode *left, *right; 
+}; 
+  
+// function to create a new interval search tree node 
+ITNode * create(Interval i, int num) 
+{ 
+    ITNode *temp = new ITNode; 
+    temp->i = new Interval(i); 
+    temp->max = i.upper;
+	temp->num = num;
+    temp->left = temp->right = NULL; 
+}; 
+
+// function to insert a new interval search tree node 
+ITNode *insert(ITNode *root, Interval i, int num) 
+{ 
+    // tree is empty, new node becomes root 
+    if (root == NULL) 
+        return create(i, num); 
+  
+    // low value of interval at root 
+    int l = root->i->lower; 
+  
+    if (i.lower < l) root->left = insert(root->left, i, num);
+    else root->right = insert(root->right, i, num);
+  
+    return root; 
+}
+
+// function to check if L is in overlap
+bool within(Interval i, int L) 
+{ 
+    if (i.lower <= L && L <= i.upper) return true; 
+    return false; 
+} 
+  
+// main function that searches a given interval i in a given interval tree 
+Interval *search(ITNode *root, int L) 
+{ 
+    // tree is empty 
+    if (root == NULL) return NULL; 
+  
+    if (within(*(root->i), L)) 
+        return root->i->num; 
+      
+    if (root->left != NULL && L < root->left) 
+        return search(root->left, L); 
+  
+    // else interval can only overlap with right subtree 
+    return search(root->right, L); 
+} 
+
 int myMarkovFunc(BELLApars& b_pars)
 {
-	// https://stackoverflow.com/questions/3286448/calling-a-python-method-from-c-c-and-extracting-its-return-value/24687260	
+	// https://stackoverflower.com/questions/3286448/calling-a-python-method-from-c-c-and-extracting-its-return-value/24687260	
   	// set PYTHONPATH to working directory
    	setenv("PYTHONPATH",".",1);
    	PyObject *pName, *pModule, *pDict, *pFunc, *pValue, *presult;
